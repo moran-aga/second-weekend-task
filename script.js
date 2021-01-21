177278321f9a481880b9f2598a576bf27f468453
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-21 14:10:48 +0200

    WEEKEND DASK
    step one - build an array + 10 object + 5 propeties each

---
+++ script.js
@@ -0,0 +1,121 @@
+"use strict"
+let tasks = [task1,task2,task3,task4,task5,task6,task7,task8,task9,task10];
+
+    let task1 = {
+        startedAt:new Date("2021-01-10:10:00"),
+
+        finishedAt: new Date("2021-01-10:13:00"),
+
+        tasksGiven: 10,
+
+        tasksFinished: 6,
+
+        topic: "HTML",
+}
+
+    let task2 = {
+        startedAt:new Date("2021-01-11:10:00"),
+    
+        finishedAt: new Date("2021-01-11:15:00"),
+    
+        tasksGiven: 15,
+    
+        tasksFinished: 8,
+    
+        topic: "HTML",
+    }
+
+    let task3 = {
+        startedAt:new Date("2021-01-12:12:00"),
+    
+        finishedAt: new Date("2021-01-12:17:00"),
+    
+        tasksGiven: 5,
+    
+        tasksFinished: 3,
+    
+        topic: "CSS",
+    }
+    
+    let task4 = {
+        startedAt:new Date("2021-01-13:13:00"),
+        
+        finishedAt: new Date("2021-01-13:19:00"),
+        
+        tasksGiven: 7,
+        
+        tasksFinished: 4,
+        
+        topic: "CSS",
+    }
+
+    let task5 = {
+        startedAt:new Date("2021-01-14:10:00"),
+        
+        finishedAt: new Date("2021-01-14:13:00"),
+        
+        tasksGiven: 25,
+        
+        tasksFinished: 12,
+        
+        topic: "JAVASCRIPT",
+        }
+    
+    let task6 = {
+        startedAt:new Date("2021-01-15:10:00"),
+        
+        finishedAt: new Date("2021-01-15:19:00"),
+        
+        tasksGiven: 35,
+        
+        tasksFinished: 20,
+        
+        topic: "JAVASCRIPT",
+        }
+        
+    let task7 = {
+        startedAt:new Date("2021-01-16:12:00"),
+            
+        finishedAt: new Date("2021-01-16:13:00"),
+            
+        tasksGiven: 4,
+            
+        tasksFinished: 4,
+            
+        topic: "JAVASCRIPT",
+        }
+    let task8 = {
+        startedAt:new Date("2021-01-17:10:00"),
+            
+        finishedAt: new Date("2021-01-10:15:00"),
+            
+        tasksGiven: 17,
+            
+        tasksFinished: 15,            
+        
+        topic: "HTML + CSS",
+        }
+        
+    let task9 = {
+        startedAt:new Date("2021-01-10:10:00"),
+
+        finishedAt: new Date("2021-01-10:14:00"),
+            
+        tasksGiven: 4,
+            
+        tasksFinished: 1,
+            
+        topic: "GITHUB",
+        }
+            
+    let task10 = {
+        startedAt:new Date("2021-01-20:10:00"),
+                
+        finishedAt: new Date("2021-01-20:18:00"),
+                
+        tasksGiven: 10,
+                
+        tasksFinished: 6,
+                
+        topic: "JAVASCRIPT",
+        }
